a3741d97b70c3021ddb5ab6290e478e87ac1393e
Michael Hudson <huddo121@gmail.com>
2017-08-26 14:03:19 +1000

    Initial commit

---
+++ init.el
@@ -0,0 +1,115 @@
+;;
+;; Variable Definitions
+;;
+
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;;
+;; Packages and Plugins
+;;
+
+;; Set up package management
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Highlight Indent Guides
+(use-package highlight-indent-guides)
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+(setq highlight-indent-guides-method 'character)
+
+;; Projectile
+(use-package projectile)
+(add-hook 'prog-mode-hook 'projectile-mode)
+
+;; Neotree
+(use-package neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+;; Solarized, dark, because I'm allergic to sunlight
+(use-package solarized-theme)
+(load-theme 'solarized-dark)
+
+;; Install Programming Languages
+(use-package elixir-mode) ;; Elixir Packages
+(use-package alchemist)
+
+;;
+;; Modes
+;;
+(electric-indent-mode 0)
+
+;;
+;; Cusom Behaviour
+;;
+
+(global-linum-mode t) ;; Line numbers, please
+
+;; Set company-mode to start after initialisation
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Save file on loss of focus
+;; TODO: Actually get this to work
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
+
+;; Switch open windows with C-c <Arrow>
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+;; Use S-/ to comment or uncomment the current line or selected region
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+(global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
